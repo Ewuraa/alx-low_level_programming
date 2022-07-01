d01f17978e04b440cf5362e823320b11b887a3ec
Ewuraa <ewurafua.k.e@gmail.com>
2022-07-01 05:20:15 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
-#include <stdio.h> 
-
-/** 
- *
- *  * main - Entry point 
- *
- *   * Description: 'Hello, puts C Program' 
- *
- *    * Return: Always 0 (sucess) 
- *
- *     */ 
-
-int main(void) 
-
-{ 
-
-	puts("\"Programming is like building a multilingual puzzle"); 
-
-	return (0); 
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Hello, puts C Program'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts ("\"Progeamming is like building a multilingual puzzle");
+	return (0)
