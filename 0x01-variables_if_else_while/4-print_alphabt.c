a58f5641f38ad5fb7e2f972b719259405eabace7
Ewuraa <ewurafua.k.e@gmail.com>
2022-07-01 17:43:55 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desription: Use `putchar` to print all letters but the letter 'q' and 'e'
+ * insude the main function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		else
+			putchar(c);
+	}
+	putchar('\n');
+
+return (0);
+}
