0b6ed6d2f04b8e75f525cc4b7d890cadcbebd28e
Ewuraa <ewurafua.k.e@gmail.com>
2022-07-05 19:30:45 -0700

    Tassk 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,15 +1,46 @@
 #include "main.h"
-
 /**
- * add - Add two integers
+ * times_table - prints the 9 times table, starting with 0.
  *
- * @a: int type number
- * @b: int type number
- *
- * Return: result of addition
+ * return - no retun.
  */
 
-int add(int a, int b)
+void times_table(void)
 {
-	return (a + b);
+	int x = 0; /* factor */
+	int y; /* count */
+	int z; /* computed value */
+
+	while (x < 10)
+	{
+		y = 0;
+		while (y < 10)
+		{
+			z = x * y;
+
+			if (z > 9)
+			{
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
+			}
+
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
 }
