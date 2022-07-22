7b7c1e413582474ac380223eba9679f5aa579327
Ewuraa <ewurafua.k.e@gmail.com>
2022-07-22 06:45:23 -0700

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,24 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - multiply two numbers
+ * main - a program that prints all arguments it receives.
  * @argc: arguement count
  * @argv: array of pointers to arguement strings
- * Return: result of multiplication or 1
+ * Return: 0
  **/
 int main(int argc, char *argv[])
 {
-	int a, b, result;
-
-	if (argc <= 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-/* atoi convert a string to an int */
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	result = a * b;
-	printf("%d\n", result);
+	while (argc--)
+		printf("%s\n", *argv++);
 	return (0);
 }
